855cda55e421481f61c317cd045ed11897ada0cf
tonyseijas <antonio.seijas@garajedeideas.com>
2025-12-05 18:51:40 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ToyVillageWorld from '../ToyVillageWorld.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToyVillageWorld />
+  </React.StrictMode>,
+)
